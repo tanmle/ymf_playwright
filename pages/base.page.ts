0f39718a346061e26004e1e645e0fa8a92be04e9
Tan Le <tanmle@outlook.com>
2025-11-17 08:46:06 +0700

    Clean gh-pages

--- pages/base.page.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from '@playwright/test';
-
-export class BasePage {
-  constructor(readonly page: Page) {}
-
-  lblSuccessMessage = this.page.locator('.swal2-title');
-  lblSuccessMessageDetail = this.page.locator('.swal2-html-container');
-}
